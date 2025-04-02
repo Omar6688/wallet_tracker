2ca9952f755bbb660a642e33aadcefd55482b30e
Omar Bitar <omarbitar88@gmail.com>
2025-04-02 13:25:19 +0200

    Add expense creation form using ModelForm and dynamic view

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='expenses'),
+    path('add/', views.add_expense, name='add_expense'),  
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense  
 
 def index(request):
     expenses = Expense.objects.all().order_by('-date')  # get all expenses
     return render(request, 'expenses/index.html', {'expenses': expenses})  # send to template
+
+from .forms import ExpenseForm  # import the form
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expenses')  # name of the index URL
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', {'form': form})
+
