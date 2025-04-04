fc0622466d0fb4ed500c106b6f07910138b0f4e8
Omar Bitar <omarbitar88@gmail.com>
2025-04-04 19:16:47 +0200

    Fix Ensure home view and URL routing for deployment

--- wallet_project/settings.py
+++ wallet_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wallet-tracker-omar.herokuapp.com']
 
 
 # Application definition
