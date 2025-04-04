879704e5b47a2b8011b179d52edb1800edfaebcc
Omar Bitar <omarbitar88@gmail.com>
2025-04-04 20:42:35 +0200

    Fix: Add Heroku domain to ALLOWED_HOSTS

--- wallet_project/settings.py
+++ wallet_project/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wallet-tracker-omar.herokuapp.com']
-
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
