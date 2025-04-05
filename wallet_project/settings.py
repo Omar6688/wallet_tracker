d014d16cb063fa5ab47ca966ff5c1c179902db8a
Omar Bitar <omarbitar88@gmail.com>
2025-04-05 18:41:55 +0200

    Fix: Correct static folder structure and settings

--- wallet_project/settings.py
+++ wallet_project/settings.py
@@ -126,10 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC FILES (CSS, JS, Images)
-STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # for local dev
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # for Heroku
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
